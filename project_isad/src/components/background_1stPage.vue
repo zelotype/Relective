98c9c725b6e9b56a8c9869c5519872ef5498fb4f
prapawity <60070045@kmitl.ac.th>
2019-03-20 15:51:27 +0700

    commit folder Isad

---
+++ project_isad/src/components/background_1stPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <div style="background-color:black;opacity:0.3;width: 100%;height: 100%;position: absolute;"></div>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  </div>
+</template>
+<script>
+export default {
+  name: 'bg_1stPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style scoped>
+.home {
+  background-image: url("../assets/banner.jpg");
+  background-attachment: fixed;
+  background-position: center center;
+  background-size: cover;
+  background-color: transparent;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -19999;
+}
+</style>
---
+++ project_isad/src/views/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <bg_1stPage></bg_1stPage>
+  </div>
+</template>
+<script>
+  import bg_1stPage from "../components/background_1stPage";
+  export default {
+    name: "MainPage",
+    components: {
+      bg_1stPage
+    },
+
+  }
+</script>
+<style scoped>
+</style>
