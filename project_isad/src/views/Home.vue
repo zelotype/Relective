12648362192513421dc1c71c6b7c952b2a23bfbe
prapawity <60070045@kmitl.ac.th>
2019-03-21 16:26:13 +0700

    process login form

--- project_isad/src/views/Home.vue
+++ project_isad/src/views/Home.vue
@@ -3,23 +3,28 @@
     <bg_1stPage></bg_1stPage>
     <!-- <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <div class="container-fluid" style="height: 100vh;padding: 0;margin-left: 0px;margin-right: 0px;">
-      <div class="row" style="width:100%;height:100%;margin: 0">
-        <div class="col-lg-8 col-12" id="white_space"></div>
-        <div class="col-lg-4 col-12" id="white_space2">
-          <div class="form" style="height:100%;width:100%;margin:auto;padding-left:50px;padding-right:50px;padding-top:10px">
-            <img src="http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F10%2Fnexus2cee_Search-Thumb-150x150.png&w=150&h=150&zc=3"
-              style="margin:auto;margin-bottom:20px">
-            <a class="login-with-guest" v-on:click="callSomeFunc2()">
-              <span class="icon fa fa-address-book"></span>
-              Login with Guest
-            </a>
-            <a class="login-with-google" id="google-signin-button">
-              <span class="icon fa fa-google-plus"></span>
-              Login with google
-            </a>
-            <a href="#" v-on:click="signOut()">Sign out</a>
-          </div>
+    <div style="padding-bottom:10%"></div>
+    <div class="container-fluid"
+      style="height: 100%;width:40%;text-align:center;background-color: #F5F5F5;border-radius:15px;box-shadow: 5px 5px 5px #404040;padding-top:20px;padding-bottom:20px">
+      <div class="row" style="width:100%;height:100%;margin: 0;">
+        <div class="col-12">
+          <img
+            src="http://www.androidpolice.com/wp-content/themes/ap2/ap_resize/ap_resize.php?src=http%3A%2F%2Fwww.androidpolice.com%2Fwp-content%2Fuploads%2F2015%2F10%2Fnexus2cee_Search-Thumb-150x150.png&w=200&h=200&zc=3"
+            style="margin:auto;margin-bottom:20px">
+        </div>
+        <div class="col-12" id="btn_login">
+          <a class="btn blue">Login With Guest</a>
+          <!-- <a class="login-with-guest" v-on:click="callSomeFunc2()">
+            <span class="icon fa fa-address-book"></span>
+            Login with Guest
+          </a> -->
+
+        </div>
+        <div class="col-12" id="btn_login">
+          <a class="btn blue g-signin2" id="my-signin2" data-onsuccess="onSignIn"></a>
+        </div>
+        <div class="col-12" id="btn_login">
+          <a href="#" v-on:click="signOut()">Sign out</a>
         </div>
       </div>
     </div>
@@ -43,14 +48,10 @@
     },
     methods: {
       callSomeFunc() {
-        this.$router.push(
-          '/InformationForm'
-        );
+        this.$router.push("/InformationForm");
       },
       callSomeFunc2() {
-        this.$router.push(
-          '/MainPage'
-        );
+        this.$router.push("/MainPage");
       },
       onSignIn(user) {
         const profile = user.getBasicProfile();
@@ -62,7 +63,24 @@
           console.log("User signed out.");
         });
       },
-    },
+      onSuccess(googleUser) {
+        console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+      },
+      onFailure(error) {
+        console.log(error);
+      },
+      renderButton() {
+        gapi.signin2.render('my-signin2', {
+          'scope': 'profile email',
+          'width': 240,
+          'height': 50,
+          'longtitle': true,
+          'theme': 'dark',
+          'onsuccess': onSuccess,
+          'onfailure': onFailure
+        });
+      }
+    }
   };
 </script>
 <style scoped>
@@ -71,7 +89,7 @@
   }
 
   body {
-    background-color: #eeeeee;
+    background-color: red;
   }
 
   img {
@@ -84,7 +102,8 @@
 
   .login-with-guest,
   .login-with-google {
-    width: 100%;
+    text-align: center;
+    width: 20%;
     height: 40px;
     display: block;
     margin: 15px 0;
@@ -103,14 +122,6 @@
     opacity: 1;
   }
 
-  .login-with-guest {
-    background: #696969;
-  }
-
-  .login-with-google {
-    background: #db4a37;
-  }
-
   .login-with-guest .icon,
   .login-with-google .icon {
     float: left;
@@ -123,24 +134,46 @@
     border-right: 1px solid #fff;
   }
 
-  #white_space {
-    padding: 0;
-    display: block;
-    height: 100%;
-    width: 100%;
-  }
-
-  #white_space2 {
-    padding: 0;
-    height: 100%;
-    background-color: white;
-  }
-
   a:hover {
     text-decoration: underline;
   }
 
-  @media (max-width: 1024px) {
+  .btn {
+    width: 90%;
+    border-radius: 5px;
+    padding: 15px 25px;
+    font-size: 22px;
+    text-decoration: none;
+    color: #fff;
+    position: relative;
+    display: inline-block;
+  }
+
+  .btn:active {
+    transform: translate(0px, 5px);
+    -webkit-transform: translate(0px, 5px);
+    box-shadow: 0px 1px 0px 0px;
+  }
+
+  .blue {
+    background-color: #55acee;
+    box-shadow: 0px 5px 0px 0px #3C93D5;
+  }
+
+  .blue:hover {
+    background-color: #6FC6FF;
+  }
+
+  #btn_login {
+    padding: 5px;
+    text-align: center;
+  }
+  .g-signin2{
+    width: 90%;
+  }
+
+
+  /* @media (max-width: 1024px) {
 
     #white_space,
     #white_space2 {
@@ -154,5 +187,5 @@
     #white_space2 {
       height: 50%;
     }
-  }
+  } */
 </style>
