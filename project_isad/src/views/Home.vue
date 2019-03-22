5526945318eaddf3b1129770a5dc74c023f83bd5
prapawity <60070045@kmitl.ac.th>
2019-03-22 12:50:50 +0700

    process form

--- project_isad/src/views/Home.vue
+++ project_isad/src/views/Home.vue
@@ -13,15 +13,29 @@
             style="margin:auto;margin-bottom:20px">
         </div>
         <div class="col-12" id="btn_login">
-          <a class="btn blue">Login With Guest</a>
-          <!-- <a class="login-with-guest" v-on:click="callSomeFunc2()">
-            <span class="icon fa fa-address-book"></span>
-            Login with Guest
-          </a> -->
-
+          <button type="button" class="google-button" v-on:click="callSomeFunc2">
+            <span class="google-button__text">Sign in with Guest</span>
+          </button>
         </div>
         <div class="col-12" id="btn_login">
-          <a class="btn blue g-signin2" id="my-signin2" data-onsuccess="onSignIn"></a>
+          <button type="button" class="google-button" id="google-signin-button">
+            <span class="google-button__icon">
+              <svg viewBox="0 0 366 372" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M125.9 10.2c40.2-13.9 85.3-13.6 125.3 1.1 22.2 8.2 42.5 21 59.9 37.1-5.8 6.3-12.1 12.2-18.1 18.3l-34.2 34.2c-11.3-10.8-25.1-19-40.1-23.6-17.6-5.3-36.6-6.1-54.6-2.2-21 4.5-40.5 15.5-55.6 30.9-12.2 12.3-21.4 27.5-27 43.9-20.3-15.8-40.6-31.5-61-47.3 21.5-43 60.1-76.9 105.4-92.4z"
+                  id="Shape" fill="#EA4335" />
+                <path
+                  d="M20.6 102.4c20.3 15.8 40.6 31.5 61 47.3-8 23.3-8 49.2 0 72.4-20.3 15.8-40.6 31.6-60.9 47.3C1.9 232.7-3.8 189.6 4.4 149.2c3.3-16.2 8.7-32 16.2-46.8z"
+                  id="Shape" fill="#FBBC05" />
+                <path
+                  d="M361.7 151.1c5.8 32.7 4.5 66.8-4.7 98.8-8.5 29.3-24.6 56.5-47.1 77.2l-59.1-45.9c19.5-13.1 33.3-34.3 37.2-57.5H186.6c.1-24.2.1-48.4.1-72.6h175z"
+                  id="Shape" fill="#4285F4" />
+                <path
+                  d="M81.4 222.2c7.8 22.9 22.8 43.2 42.6 57.1 12.4 8.7 26.6 14.9 41.4 17.9 14.6 3 29.7 2.6 44.4.1 14.6-2.6 28.7-7.9 41-16.2l59.1 45.9c-21.3 19.7-48 33.1-76.2 39.6-31.2 7.1-64.2 7.3-95.2-1-24.6-6.5-47.7-18.2-67.6-34.1-20.9-16.6-38.3-38-50.4-62 20.3-15.7 40.6-31.5 60.9-47.3z"
+                  fill="#34A853" /></svg>
+            </span>
+            <span class="google-button__text">Sign in with Google</span>
+          </button>
         </div>
         <div class="col-12" id="btn_login">
           <a href="#" v-on:click="signOut()">Sign out</a>
@@ -39,48 +53,42 @@
   export default {
     name: "home",
     components: {
-      bg_1stPage
+      bg_1stPage,
     },
-    mounted() {
-      gapi.signin2.render("google-signin-button", {
-        onsuccess: this.onSignIn
-      });
+    data() {
+      return {
+        googleSignInParams: {
+          client_id: '446125241277-rl36d6pqtnsm6h4a6i89c2d108ri5b30.apps.googleusercontent.com'
+        }
+      }
     },
     methods: {
+      onSignIn(user) {
+        const profile = user.getBasicProfile()
+      },
+      onSignInSuccess(googleUser) {
+        // `googleUser` is the GoogleUser object that represents the just-signed-in user.
+        // See https://developers.google.com/identity/sign-in/web/reference#users
+        const profile = googleUser.getBasicProfile() // etc etc
+        this.callSomeFunc()
+      },
+      onSignInError(error) {
+        // `error` contains any error occurred.
+        console.log('OH NOES', error)
+      },
       callSomeFunc() {
         this.$router.push("/InformationForm");
       },
       callSomeFunc2() {
         this.$router.push("/MainPage");
       },
-      onSignIn(user) {
-        const profile = user.getBasicProfile();
-        this.callSomeFunc();
+      mounted() {
+        gapi.signin2.render('google-signin-button', {
+          onsuccess: this.onSignIn,
+        })
       },
-      signOut() {
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-          console.log("User signed out.");
-        });
-      },
-      onSuccess(googleUser) {
-        console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-      },
-      onFailure(error) {
-        console.log(error);
-      },
-      renderButton() {
-        gapi.signin2.render('my-signin2', {
-          'scope': 'profile email',
-          'width': 240,
-          'height': 50,
-          'longtitle': true,
-          'theme': 'dark',
-          'onsuccess': onSuccess,
-          'onfailure': onFailure
-        });
-      }
     }
+
   };
 </script>
 <style scoped>
@@ -100,92 +108,68 @@
     border-radius: 50%;
   }
 
-  .login-with-guest,
-  .login-with-google {
-    text-align: center;
-    width: 20%;
-    height: 40px;
-    display: block;
-    margin: 15px 0;
-    color: #fff;
-    text-align: center;
-    line-height: 40px;
-    font-size: 14px;
-    opacity: 0.95;
-    border-radius: 3px;
-    -moz-border-radius: 3px;
-    -webkit-border-radius: 3px;
-  }
-
-  .login-with-guest:hover,
-  .login-with-google:hover {
-    opacity: 1;
-  }
-
-  .login-with-guest .icon,
-  .login-with-google .icon {
-    float: left;
-    font-size: 21px;
-    width: 50px;
-    height: 26px;
-    margin: 7px;
-    padding: 2px;
-    text-align: center;
-    border-right: 1px solid #fff;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }
-
-  .btn {
-    width: 90%;
-    border-radius: 5px;
-    padding: 15px 25px;
-    font-size: 22px;
-    text-decoration: none;
-    color: #fff;
-    position: relative;
+  .g-signin-button {
+    /* This is where you control how the button looks. Be creative! */
     display: inline-block;
+    padding: 4px 8px;
+    border-radius: 3px;
+    background-color: #3c82f7;
+    color: #fff;
+    box-shadow: 0 3px 0 #0f69ff;
   }
+</style>
+<style lang="scss" scoped>
+  .google-button {
+    height: 40px;
+    width: 100%;
+    border-width: 0;
+    background: white;
+    color: #737373;
+    border-radius: 5px;
+    white-space: nowrap;
+    box-shadow: 1px 1px 0px 1px rgba(0, 0, 0, 0.05);
+    transition-property: background-color, box-shadow;
+    transition-duration: 150ms;
+    transition-timing-function: ease-in-out;
+    padding: 0;
+    margin: 2px;
 
-  .btn:active {
-    transform: translate(0px, 5px);
-    -webkit-transform: translate(0px, 5px);
-    box-shadow: 0px 1px 0px 0px;
-  }
+    &:focus,
+    &:hover {
+      box-shadow: 1px 4px 5px 1px rgba(0, 0, 0, 0.1);
+    }
 
-  .blue {
-    background-color: #55acee;
-    box-shadow: 0px 5px 0px 0px #3C93D5;
-  }
-
-  .blue:hover {
-    background-color: #6FC6FF;
-  }
-
-  #btn_login {
-    padding: 5px;
-    text-align: center;
-  }
-  .g-signin2{
-    width: 90%;
-  }
-
-
-  /* @media (max-width: 1024px) {
-
-    #white_space,
-    #white_space2 {
-      height: 100%;
+    &:active {
+      background-color: #e5e5e5;
+      box-shadow: none;
+      transition-duration: 10ms;
     }
   }
 
-  @media (max-width: 1023px) {
+  .google-button__icon {
+    display: inline-block;
+    vertical-align: middle;
+    margin: 8px 0 8px 8px;
+    width: 18px;
+    box-sizing: border-box;
+  }
 
-    #white_space,
-    #white_space2 {
-      height: 50%;
-    }
-  } */
+  .google-button__icon--plus {
+    width: 27px;
+  }
+
+  .google-button__text {
+    display: inline-block;
+    vertical-align: middle;
+    padding: 0 24px;
+    font-size: 14px;
+    font-weight: bold;
+    font-family: 'Roboto', arial, sans-serif;
+  }
+
+  // Boilerplate stuff
+
+  button~button {
+    margin-left: 20px;
+  }
 </style>
