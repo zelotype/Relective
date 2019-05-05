94ae5d95496c56cec40f4453d11dff4877192ec6
prapawity <60070045@kmitl.ac.th>
2019-05-05 18:34:51 +0700

    dellall

--- Relective-after_firebase/project_isad/src/app.js
+++
@@ -1,8 +0,0 @@
-
-import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'node_modules/bootstrap/scss/bootstrap';
-import 'node_modules/bootstrap-vue/src/index.scss';
-Vue.use(BootstrapVue)
